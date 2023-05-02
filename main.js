8b727fbd5bda55b28f2f357e533b8d22e94489d8
mamunmr1 <mamun.mr66@gmail.com>
2023-05-03 00:04:35 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function multiplicationTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    const product = num * i;
+    console.log(`${num} x ${i} = ${product}`);
+  }
+}
+multiplicationTable(1)
+multiplicationTable(2)
+multiplicationTable(11)
